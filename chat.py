6a47cef3a5605f6d2ad358f28adb002c22645e44
hs.cho <hbbread.401@gmail.com>
2025-12-29 05:46:03 +0000

    initial

---
+++ chat.py
@@ -0,0 +1,36 @@
+#pip install streamlit
+import os
+import streamlit as st
+from llm import get_ai_response
+
+st.set_page_config(page_title="소득세 챗봇", page_icon="🐵")
+st.title("🐵 소득세 챗봇")
+st.caption("소득세에 관련된 모든것을 답변합니다")
+
+
+
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+print(f"before === {st.session_state.message_list}")
+
+for message in st.session_state.message_list:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
+
+if user_question := st.chat_input(placeholder="소득세에 대해 궁금한 내용을 말씀해주세요"):
+    with st.chat_message("user"):
+        st.write(user_question) 
+    st.session_state.message_list.append({"role": "user", "content": user_question})
+
+    with st.spinner("답변을 생성하는 중입니다."):
+        ai_response = get_ai_response(user_question)
+
+        with st.chat_message("AI"):
+            ai_message = st.write_stream(ai_response) 
+            st.session_state.message_list.append({"role": "ai", "content": ai_message})
+        
+print(f"after === {st.session_state.message_list}")
+
+
+# few-shot 으로 답변의 예시를 줘서 성능을 더 올린다. 
